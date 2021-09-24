8aa67519e4e4d4089980aedba08de61d20694c80
Ana Pereira <anaferraspereira@gmail.com>
2021-09-24 11:22:16 +0100

    Fix button's symbol positioning

--- src/LocationButton.ts
+++ src/LocationButton.ts
@@ -37,7 +37,10 @@ export default class LocationButton {
     // eslint-disable-next-line max-len
     controlOutterSymbol.src = `data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20height%3D%2224px%22%20viewBox%3D%220%200%2024%2024%22%20width%3D%2224px%22%20fill%3D%22%23${buttonStyle?.symbolColor?.replace('#', '') ?? '6F6F6F'}%22%3E%3Cpath%20d%3D%22M0%200h24v24H0z%22%20fill%3D%22none%22%2F%3E%3Cpath%20d%3D%22M20.94%2011c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83%203.52%203.52%206.83%203.06%2011H1v2h2.06c.46%204.17%203.77%207.48%207.94%207.94V23h2v-2.06c4.17-.46%207.48-3.77%207.94-7.94H23v-2h-2.06zM12%2019c-3.87%200-7-3.13-7-7s3.13-7%207-7%207%203.13%207%207-3.13%207-7%207z%22%2F%3E%3C%2Fsvg%3E`;
     controlOutterSymbol.style.height = '75%';
-    controlOutterSymbol.style.margin = 'auto';
+    controlOutterSymbol.style.position = 'absolute';
+    controlOutterSymbol.style.top = '50%';
+    controlOutterSymbol.style.left = '50%';
+    controlOutterSymbol.style.transform = 'translateY(-50%) translateX(-50%)';
     this.controlUI.appendChild(controlOutterSymbol);
 
     this.controlInnerSymbol = document.createElement('div');
@@ -46,8 +49,9 @@ export default class LocationButton {
     this.controlInnerSymbol.style.width = '35%';
     this.controlInnerSymbol.style.borderRadius = '50%';
     this.controlInnerSymbol.style.position = 'absolute';
-    this.controlInnerSymbol.style.top = '32%';
-    this.controlInnerSymbol.style.left = '32%';
+    this.controlInnerSymbol.style.top = '50%';
+    this.controlInnerSymbol.style.left = '50%';
+    this.controlInnerSymbol.style.transform = 'translateY(-50%) translateX(-50%)';
 
     this.controlUI.appendChild(this.controlInnerSymbol);
 
